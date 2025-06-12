0ed0e3e71b54e60d1c2ce3c10316947ef0fbbe1c
motunrayonak <motunrayonak@gmail.com>
2025-06-12 16:44:10 +0100

    datatype created

---
+++ 002DataTypes.js
@@ -0,0 +1,30 @@
+let firstname = "Raodat"
+let lastname = " Onakomaya"
+
+let name = firstname + lastname
+console.log (name, typeof name);
+
+let num1 =7.8
+console.log (num1,typeof num1);
+
+let bool = 5>6
+console.log (bool, typeof bool);
+
+let user = null
+console.log(user, typeof user);
+
+let item
+console.log (item, typeof item);
+
+let bignum = 234689107n
+console.log(bignum, typeof bignum);
+
+let person = {name:"motunrayo",age:18}
+console.log(person, typeof person);
+
+let fruits = ["apple","banana","mango"]
+console.log (fruits,typeof fruits);
+
+function greet() {
+    return "Hello!";
+}
