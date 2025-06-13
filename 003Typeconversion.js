98de51a09c9b2baf92f3d3098b6214354e4ca131
motunrayonak <motunrayonak@gmail.com>
2025-06-13 12:45:32 +0100

    conversion created

---
+++ 003Typeconversion.js
@@ -0,0 +1,29 @@
+let boolVal = true;
+let boolToStr = String (boolVal);
+console.log(boolToStr, typeof boolToStr);
+
+let age = String (21);
+console.log(age, typeof age);
+
+let strnum = "123";
+let num1 = Number (strnum);
+console.log(num1, typeof num1);
+
+let validstr = "abc";
+let num2 = Number (validstr);
+console.log(num2,typeof num2);
+
+console.log(Boolean(undefined));
+
+console.log(Boolean(1));
+
+let value1 ="30.55";
+console.log (parseInt(value1));
+
+console.log(parseFloat(value1));
+
+let str = "10";
+let converted = +str;
+console.log(converted,typeof converted);
+
+console.log ("10"+3);
